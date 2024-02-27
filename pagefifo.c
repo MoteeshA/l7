f0a040bbaf6084ca334db226a81ee50097f238be
MoteeshA <154773678+MoteeshA@users.noreply.github.com>
2024-02-27 22:18:30 +0530

    pagefifo.c

--- pagefifo.c
+++ pagefifo.c
@@ -1,48 +1,48 @@
 #include <stdio.h>
 
-int page[50], frame[10];
+int page[10],frame[10];
 
-int main() {
-    int n, fn;
+int main(){
+	int n;
+	int j=0;
+	int i;
+	int count=0;
+	printf("enter ht number of pages: ");
+	scanf("%d",&n);
 
-    printf("Enter the number of pages: ");
-    scanf("%d", &n);
-
-    printf("\nEnter replacement string: ");
-    for (int i = 0; i < n; i++)
-        scanf("%d", &page[i]);
-
-    printf("\nEnter the number of frames: ");
-    scanf("%d", &fn);
-
-    printf("\nPAGE STRING | FRAMES\t\t| PAGE FAULTS\n");
-    printf("----------------------------------------\n");
-
-    int pagefault = 0;
-    int inFrame;
-
-    for (int i = 0; i < n; i++) {
-        inFrame = 0;
-        printf("%d\t\t| ", page[i]);
-
-        for (int j = 0; j < fn; j++) {
-            printf("%d ", frame[j]);
-            if (frame[j] == page[i]) {
-                inFrame = 1;
-                break;
-            }
-        }
-
-        if (!inFrame) {
-            int j = 0;
-            frame[j] = page[i];
-            pagefault++;
-        }
-
-        printf("\t\t| %d\n", pagefault);
-    }
-
-    printf("\nTotal Page Faults: %d\n", pagefault);
-
-    return 0;
+	printf("\nEntert the referance string");
+	for(int i=0;i<n;i++) 
+	scanf("%d",&page[i]);
+	int fn;
+	printf("\nEnter thr number of frames:");
+	scanf("%d",&fn);
+	
+	
+	for(int i=0;i<fn;i++)
+		frame[i]=-1;
+	int faults=0;
+	printf("PAGE\tPAGEFAULT\n");
+    
+	for(int i=0;i<n;i++){
+		printf("%d\t\t",page[i]);
+		
+		int count=0;
+		for(int k=0;k<fn;k++){
+			if(frame[k]!=page[i]){
+				count++;
+			}
+	}
+	if (count==fn){
+		
+		frame[j]=page[i];
+		j=(j+1)%fn;
+		faults++;
+	}
+	for(int k=0;k<fn;k++){
+		printf("%d\t",frame[k]);
+	}
+	printf("\n");
+	}
+	printf("faults: %d",faults);
+	return 0;
 }
